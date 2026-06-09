6c826ac49506e3ca79280533deec4add19faa1e0
Sawaya Msangi <24bia050@gmail.com>
2026-06-09 10:29:59 +0300

    fix render database setup

--- voting_project/settings.py
+++ voting_project/settings.py
@@ -89,16 +89,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'voting_project.wsgi.application'
 
 # ======================
-# DATABASE (POSTGRESQL - RENDER)
+# DATABASE (MYSQL - LOCAL)
 # ======================
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'voting_app',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
 }
-
 # ======================
 # PASSWORDS
 # ======================

497079efe9134cded08061bf2200ec06c46be3cd
Sawaya Msangi <24bia050@gmail.com>
2026-06-09 16:10:29 +0300

    restore frontend homepage

--- voting_project/urls.py
+++ voting_project/urls.py
@@ -4,6 +4,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html')),  # 🔥
+
+    # 🔥 REAL FRONTEND PAGE
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+
     path('api/', include('api.urls')),
 ]
